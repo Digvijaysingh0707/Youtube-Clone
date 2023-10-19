229c9095930e270d6189de37c6c6c0c80be752c2
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-10-20 00:52:15 +0530

    added sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen)
+  if (!isMenuOpen) return null
   return (
+
+
     <div className='p-5 shadow-lg w-48'>
-       <ul>
-        <li>Home</li>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>Shorts</li>
         <li>Video</li>
         <li>Live</li>
@@ -24,6 +32,8 @@ const Sidebar = () => {
         <li>Movies</li>
       </ul>
     </div>
+
+
   )
 }
 
