defbe740fc9c8199aaf679fdb4eadb57736ea23c
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-10-20 21:43:45 +0530

    redux part

---
+++ src/utils/store.js
@@ -0,0 +1,16 @@
+import appSlice from "./appSlice";
+import chatSlice from "./chatSlice";
+import searchSlice from "./searchSlice";
+
+const { configureStore } = require("@reduxjs/toolkit");
+
+const store = configureStore({
+  reducer: {
+    app: appSlice,
+    search: searchSlice,
+    chat:chatSlice
+  }
+
+})
+
+export default store
